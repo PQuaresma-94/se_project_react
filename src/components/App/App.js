0f7c35b5fbe51a739740488314921da6b819dd78
PQuaresma-94 <pedromquaresma@gmail.com>
2023-11-10 16:53:17 -0800

    Create Header Component

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+//import components
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Foorter/>
+      <ModalWithForm/>
+      <ItemModal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+//Add files that need to be imported
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <div>
+                    <img src="" alt="Logo"/> 
+                    <h2>Date and Location</h2>
+                </div>
+                <div>
+                    <div>
+                        <button>+ Add Clothes</button>
+                    </div>
+                    <div>
+                        <h2>Terrence Tegegne</h2>
+                        <img src="" alt="Avatar"/>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header
