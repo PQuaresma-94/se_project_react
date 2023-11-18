5d06b1e777b5fe6402e43163afc68ceac9ab0f1e
PQuaresma-94 <pedromquaresma@gmail.com>
2023-11-17 19:02:25 -0800

    Filter cards using WeatherAPI temp

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,25 @@
 import ItemCard from "../ItemCard/ItemCard";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import {defaultClothingItems} from "../../utils/Constants"
+import { useMemo } from "react";
 import "./Main.css";
 
 
 const Main = ({weatherTemp, onSelectCard}) => {
+    const weatherType = useMemo(() => {
+        if (weatherTemp >= 86) {
+            return 'hot';
+          } else if (weatherTemp >= 66 && weatherTemp <= 85) {
+            return 'warm';
+          } else if (weatherTemp <= 65) {
+            return 'cold';
+          }
+    }, [weatherTemp]);
+
+    const filteredCards = defaultClothingItems.filter((item) => {
+        return item.weather.toLowerCase() === weatherType;
+    });
+
     return (
         <div>
             <main className="main">
@@ -12,7 +27,7 @@ const Main = ({weatherTemp, onSelectCard}) => {
                 <section className="cards" id="card-section">
                     <div className="cards__information">Today is {weatherTemp}° F / You may want to wear:</div>
                     <div className="cards__items">
-                        {defaultClothingItems.map((item) => (
+                        {filteredCards.map((item) => (
                             <ItemCard key={item._id} item={item} onSelectCard={onSelectCard} />
                         ))}
                     </div>
--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -1,12 +1,3 @@
-// if (temperature >= 86) {
-//     return 'hot';
-//   } else if (temperature >= 66 && temperature <= 85) {
-//     return 'warm';
-//   } else if (temperature <= 65) {
-//     return 'cold';
-//   } 
-
-
 const APIkey = "21f89b5fc71f52a8e6dc6c81dc95cd2b";
 const latitude = 44.34;
 const longitude = 10.99;
