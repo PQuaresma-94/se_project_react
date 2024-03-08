2da70857bc6ad4ef217e1fa186399cc6d3c2dfb3
PQuaresma-94 <pedromquaresma@gmail.com>
2024-03-07 17:18:04 -0800

    Add owner filter to clothing Items

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ItemCard from '../ItemCard/ItemCard';
+import { useContext } from "react";
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import './ClothesSection.css';
 
 const ClothesSection = ({ onCreateModal , onSelectCard, clothingItems, isLoggedIn, onCardLiked }) => {
+    const {currentUser} = useContext(CurrentUserContext);
+
+    const filteredCards = clothingItems.filter((item) => {
+        return item.owner === currentUser._id;
+    });
+    
     return (
         <section className="clothes" id="clothes-section">
             <div className="clothes__header">
@@ -10,7 +18,7 @@ const ClothesSection = ({ onCreateModal , onSelectCard, clothingItems, isLoggedI
                 <button className="clothes__add-button" type="text" onClick={onCreateModal}>+ Add new</button>
             </div>
             <div className="clothes__items">
-                {clothingItems.map((item) => (
+                {filteredCards.map((item) => (
                     <ItemCard key={item._id} item={item} onSelectCard={onSelectCard} isLoggedIn={isLoggedIn} onCardLiked={onCardLiked}/>
                 ))}
             </div>
