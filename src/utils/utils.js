6717e4cf233a3cc9acc6b368dfcd1c6cf0dd2396
PQuaresma-94 <pedromquaresma@gmail.com>
2024-04-08 19:39:42 -0700

    Update backend base url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://pedrowtwr.jumpingcrab.com"
+    ? "https://api.pedrowtwr.jumpingcrab.com"
     : "http://localhost:3001";
 
 export const processServerResponse = (res) => {
